399514a051770f15405c42feb093cab55b5ce7db
Abdelrhman-Azab <50024540+Abdelrhman-Azab@users.noreply.github.com>
2021-09-04 15:19:07 +0200

    Update singleton.dart

--- singleton.dart
+++ singleton.dart
@@ -1,5 +1,7 @@
 //Singleton : ensures a class has only one inistance and provides a global point of access to it
 
+//lazy singleton will created whenever we need it and will be removed whenever it isn't necessary 
+
 class Singleton {
   //Private static inistance only that inistance we can create and user on the app
   static Singleton? _inistance;
